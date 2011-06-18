e83029056b147c267d22056ec99717c6422b1146
Steve Glass <s.glass@nicta.com.au>
2011-06-18 22:08:57 +1000

    Make metric default to OFDM when data not present in file. Skeleton for boost option parsing (disabled).

--- src/elc/main.cpp
+++ src/elc/main.cpp
@@ -11,17 +11,20 @@
 #include <metric_demux.hpp>
 #include <metric_group.hpp>
 #include <metric.hpp>
+#include <net/buffer_info.hpp>
 #include <net/wnic.hpp>
 #include <net/wnic_encoding_fix.hpp>
 #include <net/wnic_wallclock_fix.hpp>
 #include <dot11/frame.hpp>
 
+#include <boost/program_options.hpp>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
 #include <string.h>
 #include <unistd.h>
 
+using namespace boost::program_options;
 using namespace dot11;
 using namespace net;
 using namespace metrics;
@@ -30,6 +33,28 @@ using namespace std;
 int
 main(int ac, char **av)
 {
+   try {
+
+#if 0
+   const char *what = NULL;
+   uint16_t rts_cts_threshold = UINT16_MAX;
+
+   options_description options("program options");
+   desc.add_options()
+      ("help,h", "produce this help message")
+      ("input,i", value<string>()->default_value("mon0"), "input file/device name")
+      ("rts-threshold,r", value<uint16_t>()->default_value(UINT16_MAX), "RTS threshold level")
+      ;
+
+   variables_map vars;       
+   store(parse_command_line(ac, av, options), vars);
+   notify(vars);   
+
+   if(vars.count("help")) {
+      cout << options << "\n";
+      return EXIT_SUCCESS;
+   }
+#else
    opterr = 0;
    int opt, errs = 0;
    uint16_t metric = 0;
@@ -52,18 +77,19 @@ main(int ac, char **av)
       cerr << "usage: elc [-r rts_cts_threshold] -i input" << endl;
       exit(EXIT_FAILURE);
    }
+#endif
 
-   metric_group_sptr proto(new metric_group);
-   proto->add(metric_sptr(new elc_metric(rts_cts_threshold)));
-   proto->add(metric_sptr(new elc_mrr_metric(rts_cts_threshold)));
-   proto->add(metric_sptr(new ett_metric));
-   metric_sptr m(metric_sptr(new metric_demux(proto)));
+   	metric_group_sptr proto(new metric_group);
+      proto->add(metric_sptr(new elc_metric(rts_cts_threshold)));
+      proto->add(metric_sptr(new elc_mrr_metric(rts_cts_threshold)));
+      proto->add(metric_sptr(new ett_metric));
+      metric_sptr m(metric_sptr(new metric_demux(proto)));
 
-   try {
       wnic_sptr w(wnic::open(what));
       w = wnic_sptr(new wnic_wallclock_fix(w));
-      w = wnic_sptr(new wnic_encoding_fix(w, 0));
+      w = wnic_sptr(new wnic_encoding_fix(w, CHANNEL_CODING_OFDM | CHANNEL_PREAMBLE_LONG));
 #if 0
+      // ToDo: find out why this is garbaging inbound data!
       w->filter("wlan type data"); // ToDo: add BPF test for outbound-only frames
 #endif
       buffer_sptr b(w->read());
@@ -89,6 +115,9 @@ main(int ac, char **av)
             tick = timestamp;
          }
       }
+   } catch(const error& x) {
+      // ToDo: generate a usage message?
+      cerr << x.what() << endl;
    } catch(const exception& x) {
       cerr << x.what() << endl;
    } catch(...) {
