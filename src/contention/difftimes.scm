5f7060eafa8c6cc125f47cced496d5e5ea7e00bb
Steve Glass <s.glass@nicta.com.au>
2011-08-25 11:09:32 +1000

    Modified difftimes.

--- src/contention/difftimes.scm
+++ src/contention/difftimes.scm
@@ -1,11 +1,13 @@
 #!/usr/bin/guile
 !#
 
-(let ((first (read)))
-  (let (filter (l first) (last 0))
-    (cond ((eof-object? l)
-           #f)
-          ((and (list? l) (= (length l) 3))
-           (display (list l (- (cadr l) last)))
-           (filter (read) (cadr 1))))))
-        
+(call/cc
+ (lambda (break)
+   (let ((first (read)))
+     (let filter ((l first) (last 0))
+       (cond ((and (list? l) (= (length l) 3))
+              (display (cons (- (cadr l) last) l))
+              (newline)
+              (filter (read) (cadr l)))
+             (else
+              (break l)))))))
