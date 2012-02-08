d2261a40b6e6da7f745b4fc6461095c1f9f6fd76
Steve Glass <s.glass@nicta.com.au>
2012-02-08 12:16:18 +1000

    Fix bug.

--- src/elc/legacy_elc_metric.cpp
+++ src/elc/legacy_elc_metric.cpp
@@ -86,8 +86,16 @@ legacy_elc_metric::add(buffer_sptr b)
    if(DATA_FRAME == fc.type() && info->has(TX_FLAGS)) {
       bool tx_success = (0 == (info->tx_flags() & TX_FLAGS_FAIL));
       if(tx_success && info->channel_encoding() == enc_) {
+
+         const uint32_t IEEE80211_HDR_SZ = 26;
+         const uint32_t LLC_HDR_SZ = 8;
+         const uint32_t IP_HDR_SZ = 20;
+         const uint32_t UDP_HDR_SZ = 8;
+         const uint32_t CRC_SZ = 4;
+         const uint16_t HDR_SZ = IEEE80211_HDR_SZ + LLC_HDR_SZ + IP_HDR_SZ + UDP_HDR_SZ;
+         
          ++packets_;
-         packet_octets_ += b->data_size() + CRC_SZ;
+         packet_octets_ += b->data_size() - HDR_SZ;
          rates_Kbs_sum_ += info->rate_Kbs();
       }
       frames_ += info->has(DATA_RETRIES) ? 1 + info->data_retries() : 1;
@@ -103,15 +111,10 @@ legacy_elc_metric::clone() const
 double
 legacy_elc_metric::compute(uint32_t ignored_delta_us)
 {
-   const uint32_t IEEE80211_HDR_SZ = 26;
-   const uint32_t LLC_HDR_SZ = 8;
-   const uint32_t IP_HDR_SZ = 20;
-   const uint32_t UDP_HDR_SZ = 8;
-   const uint16_t HDR_SZ = IEEE80211_HDR_SZ + LLC_HDR_SZ + IP_HDR_SZ + UDP_HDR_SZ;
 
    const double FRMS = frames_;
    const double PKTS = packets_;
-   const double AVG_PKT_SZ = (packet_octets_ - HDR_SZ) / PKTS;
+   const double AVG_PKT_SZ = packet_octets_ / PKTS;
    const double AVG_PKT_RATE_Kbs = rates_Kbs_sum_ / PKTS;
 
    /* ToDo: Computing the average rate (EMT) here is done the
