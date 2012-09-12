ae0723bad11d0ca6a9b716fbb373b0aeeb8fc275
Steve Glass <stevie@sedition.org.au>
2012-09-12 21:41:56 +1000

    Change how we account for beacon + stall time.

--- src/beacons/beacons.cpp
+++ src/beacons/beacons.cpp
@@ -30,15 +30,12 @@ main(int ac, char **av)
 {
    try {
 
-      uint16_t cw;
       uint64_t runtime;
       string what, ta_str;
-      bool debug, verbose;
+      bool verbose;
       options_description options("program options");
       options.add_options()
          ("help,?", "produce this help message")
-         ("cw,c", value<uint16_t>(&cw)->default_value(0), "CW size")
-         ("debug,g", value<bool>(&debug)->default_value(false)->zero_tokens(), "enable debug")
          ("input,i", value<string>(&what)->default_value("mon0"), "input file/device name")
          ("runtime,u", value<uint64_t>(&runtime)->default_value(0), "finish after n seconds")
          ("ta,a", value<string>(&ta_str)->default_value("00:0b:6b:0a:82:34"), "transmitter address")
@@ -59,35 +56,31 @@ main(int ac, char **av)
       w = wnic_sptr(new wnic_timestamp_swizzle(w));
       w = wnic_sptr(new wnic_timestamp_fix(w));
 
-      const uint32_t AIFS = 43; // ToDo: recover me from encoding!
-      const uint32_t CW = 67.5; // ToDo: use encoding to compute me!
-      uint_least32_t dead_time = 0;
       eui_48 ta(ta_str.data());
       buffer_sptr b(w->read());
+      uint_least32_t dead_time = 0;
       if(b) {
-         buffer_sptr p = b;
+         buffer_sptr p = b, l;
          uint64_t tick_time = UINT64_C(1000000);
          uint64_t end_time = runtime ? b->info()->timestamp_wallclock() + (runtime * tick_time) : UINT64_MAX;
          for(uint32_t n = 2; (b = buffer_sptr(w->read())) && (b->info()->timestamp_wallclock() <= end_time); p = b, ++n){
             frame curr(b);
             frame prev(p);
             if((curr.fc().subtype() == MGMT_BEACON) && (curr.address2() == ta)) {
-               uint32_t ifs = b->info()->timestamp1() - p->info()->timestamp2();
-               uint32_t dead = ifs - AIFS; // - CW; // NB beacon contention unaffected by CW clamping!
-               dead_time += dead;
                if(verbose) {
+                  uint32_t ifs = b->info()->timestamp1() - p->info()->timestamp2();
                   cout << n << " B " << ifs << endl;
                }
+               l = p;
             }
             if((prev.fc().subtype() == MGMT_BEACON) && (prev.address2() == ta)) {
-               uint32_t ifs = b->info()->timestamp1() - p->info()->timestamp2();
-               uint32_t dead = ifs - AIFS; // - (cw ? cw : CW);
-               dead_time += dead;
                if(verbose) {
-                  cout << n - 1 << " A " << dead << endl;
+                  uint32_t ifs = b->info()->timestamp1() - p->info()->timestamp2();
+                  cout << n - 1 << " A " << ifs << endl;
                }
-               if(debug && curr.fc().subtype() == MGMT_BEACON)
-                  cout << curr.address2() << endl;
+               if(l) {
+                  dead_time += b->info()->timestamp1() - l->info()->timestamp2();
+               }
             }
          }
       }
